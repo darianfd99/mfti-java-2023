af449caa5c247334f1a8291e30290fd04113dbc6
darianfd99 <darianfd99@gmail.com>
2023-10-22 19:21:18 +0300

    feat(dz2) add abstract method "createTrade" in enum "TradeType"

--- dz2/excercise1/src/Trade/TradeType.java
+++ dz2/excercise1/src/Trade/TradeType.java
@@ -1,10 +1,37 @@
 package Trade;
 
+import Car.Hatchback;
 import Car.Sedan;
+import Car.Suv;
+import Car.Pickup;
+import Car.Car;
 
 public enum TradeType {
-    HATCHBACK,
-    SEDAN,
-    SUV,
-    PICKUP,
+    HATCHBACK {
+        @Override
+        public Car createTrade(float price) {
+            return new Hatchback(price);
+        }
+    },
+    SEDAN{
+        @Override
+        public Car createTrade(float price) {
+            return new Sedan(price);
+        }
+    },
+
+    SUV{
+        @Override
+        public Car createTrade(float price) {
+            return new Suv(price);
+        }
+    },
+    PICKUP{
+        @Override
+        public Car createTrade(float price) {
+            return new Pickup(price);
+        }
+    };
+
+    public abstract Car createTrade(float price);
 }
